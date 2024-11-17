262384ff6be53c5e56111ed780747f5c30a8a951
Matias <mattiochoax@gmail.com>
2024-11-17 12:52:58 +0100

    Adding token to authorization headers in every call related to projects

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,14 @@ const api = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
 }); // creamos una instancia de axios con la url base de la api para no tener que escribirla en cada petición
 
+api.interceptors.request.use((config) => {
+	const token = localStorage.getItem("AUTH_TOKEN");
+
+	if (token) {
+		config.headers.Authorization = `Beared ${token}`;
+	}
+
+	return config;
+}); // interceptamos el token antes de cada request y si existe entonces enviamos la autorizacion con headers de autorizacion
+
 export default api;
